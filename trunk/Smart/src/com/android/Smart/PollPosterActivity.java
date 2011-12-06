5981860eed64b698da6df07362b43c906b2e1a25
hubowen@gmail.com <hubowen@gmail.com@00afb196-d371-66d7-01bb-b02bc99e4466>
2011-12-06 00:40:35 +0000

    Poll Poster -> login after user selects a poll option.

--- trunk/Smart/src/com/android/Smart/PollPosterActivity.java
+++ trunk/Smart/src/com/android/Smart/PollPosterActivity.java
@@ -1,7 +1,11 @@
 package com.android.Smart;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -33,7 +37,21 @@ public class PollPosterActivity extends Activity {
 		
 		int i = 0;
 		for (PollOption option : poster.getOptions()) {
-			buttons[i++].setText(option.getText());
+			buttons[i].setText(option.getText());
+			buttons[i].setOnClickListener(new View.OnClickListener() {
+	             public void onClick(View v) {
+	            	 AlertDialog alertDialog = new AlertDialog.Builder(PollPosterActivity.this).create();  
+					  alertDialog.setTitle("Thanks for your input!");
+					  alertDialog.setButton("OK", new DialogInterface.OnClickListener() {  
+						  public void onClick(DialogInterface dialog, int which) {  
+							  Intent intent = new Intent(PollPosterActivity.this, login.class);
+							  startActivity(intent);
+							  PollPosterActivity.this.finish();
+						  } });
+					  alertDialog.show();
+	             }
+	         });
+			i++;
 		}
 		for (; i < 9; i++) { // only 9 buttons!
 			buttons[i].setEnabled(false);
